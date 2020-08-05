a82de95013bff5a25fd25000b81fc60676e661f7
Artin Mirzaian <mirzaian@kth.se>
2020-08-05 18:15:58 +0200

    test

--- server/functions/index.js
+++ server/functions/index.js
@@ -10,4 +10,13 @@ exports.addMessage = functions.https.onRequest(async (req, res) => {
     res.send({result: `Hello World!`});
   });
 
+
+exports.createGame = functions.https.onRequest(async (req, res) => {
+  // todo
+  // Random a 4 number game id
+  // Create game in database 
+  let id = 9999
+  const snapshot = await admin.database().ref('/games/'+id).set({players: 0});
+  res.send({gameID: 9999})
+})
   
